3c8f5588732b3c82581f0fccf76dbbef135e132d
keiyuki2 <tuvsheenee916@gmail.com>
2025-08-20 16:48:47 +0800

    Update background.js

--- background.js
+++ background.js
@@ -6,7 +6,19 @@ chrome.action.onClicked.addListener((tab) => {
       { action: 'toggle_ui' },
       (response) => {
         if (chrome.runtime.lastError) {
-          console.error('Error sending message:', chrome.runtime.lastError);
+          console.error('Error:', chrome.runtime.lastError);
+          // If content script isn't injected yet, inject it
+          chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            files: ['content-script.js']
+          }).then(() => {
+            // Try again after injection
+            setTimeout(() => {
+              chrome.tabs.sendMessage(tab.id, { action: 'toggle_ui' });
+            }, 100);
+          }).catch(err => {
+            console.error('Failed to inject content script:', err);
+          });
         } else {
           console.log('Response from content script:', response);
         }
