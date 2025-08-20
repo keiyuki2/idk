35e892b0250e0a84d7ee01edab5b9f101ed91ab4
keiyuki2 <tuvsheenee916@gmail.com>
2025-08-20 16:36:24 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,16 @@
 chrome.action.onClicked.addListener((tab) => {
   if (tab.id) {
-    chrome.tabs.sendMessage(tab.id, { action: 'toggle_ui' });
+    console.log('Extension icon clicked, sending message to tab:', tab.id);
+    chrome.tabs.sendMessage(
+      tab.id, 
+      { action: 'toggle_ui' },
+      (response) => {
+        if (chrome.runtime.lastError) {
+          console.error('Error sending message:', chrome.runtime.lastError);
+        } else {
+          console.log('Response from content script:', response);
+        }
+      }
+    );
   }
 });
